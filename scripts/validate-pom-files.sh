b8ffdb96c9ecb37b2db22752d3d43d6dbb16887f
Marcos Pereira <marcos.silva@gmail.com>
2018-09-27 15:37:10 -0400

    Fix pom generation when releasing new version

---
+++ scripts/validate-pom-files.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sbt ++${TRAVIS_SCALA_VERSION} publishM2
+
+# Downloads maven schema file which will be used to validate the generated pom file
+wget -c http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd
+
+# Use xmllint to validate the generated pom.xml
+find . -name *.pom -exec xmllint --noout -schema maven-4.0.0.xsd {} \;
