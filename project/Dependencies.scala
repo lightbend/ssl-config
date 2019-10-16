b38359ea794fde3076a0120a333cc1b6ba17ec0d
Scala Steward <me@scala-steward.org>
2019-10-16 19:59:24 +0200

    Update specs2-core, specs2-junit, ... to 4.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Version {
   // Because of https://github.com/sbt/sbt/issues/4609
   val parserCombinators211 = "1.1.1"
   val parserCombinators = "1.1.2"
-  val specs2          = "4.7.1"
+  val specs2          = "4.8.0"
 
   val scala211       = "2.11.12"
   val scala212       = "2.12.8"
