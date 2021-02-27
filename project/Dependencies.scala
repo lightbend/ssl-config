17bc68be75c1171c7d5f192c035a6b4a6425680e
Scala Steward <me@scala-steward.org>
2021-02-27 01:04:04 +0100

    Update scala-library to 2.12.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Version {
   val specs2          = "4.8.1"
 
   val scala211       = "2.11.12"
-  val scala212       = "2.12.10"
+  val scala212       = "2.12.13"
   val scala213       = "2.13.1"
 }
 
