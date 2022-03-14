eb3ee155bf04b85c773b770ae76c535dad6e88af
Scala Steward <me@scala-steward.org>
2022-03-14 12:26:47 +0100

    Update scala3-library to 3.0.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Version {
   val scala211       = "2.11.12"
   val scala212       = "2.12.14"
   val scala213       = "2.13.8"
-  val scala3         = "3.0.1"
+  val scala3         = "3.0.2"
 }
 
 object Library {
