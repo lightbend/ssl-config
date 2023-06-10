6c6c7aea5787b05dbe2e19d3a25b0cd7314bc8c8
Scala Steward <scala_steward@virtuslab.com>
2023-06-10 18:22:05 +0000

    Update scala-library to 2.13.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Version {
 
   val scala211       = "2.11.12"
   val scala212       = "2.12.15"
-  val scala213       = "2.13.8"
+  val scala213       = "2.13.11"
   val scala3         = "3.0.2"
 }
 
